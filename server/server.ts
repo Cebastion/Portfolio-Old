bb24c54dd9a757554c2fff7a68c247e1989b734a
Cebastion <kdimon2006@gimal.com>
2024-03-24 07:31:06 +0200

    new commit

--- server/server.ts
+++ server/server.ts
@@ -10,7 +10,7 @@ import { FireBaseService } from "./service/firebase.service";
 //import { IOffer } from "./interface/offer.interface";
 
 const db = new MongoDb();
-const firebase = new FireBaseService()
+const firebase = new FireBaseService();
 const app = express();
 const upload = multer();
 
@@ -20,8 +20,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/works", async (req: Request, res: Response) => {
-    const works = await db.GetWorks();
-    res.json(works)
+    try {
+        await db.connect();
+        const works = await db.GetWorks();
+        res.json(works);
+    } finally {
+        await db.disconnect();
+    }
 });
 
 /*app.get("/offer/:_id", async (req: Request, res: Response) => {
@@ -35,12 +40,20 @@ app.get("/works", async (req: Request, res: Response) => {
     res.json(offer)
 });*/
 
-app.post("/add_work", upload.single("img"), async (req: Request, res: Response) => {
+app.post(
+    "/add_work",
+    upload.single("img"),
+    async (req: Request, res: Response) => {
         const data: IWork = req.body;
         const file = req.file;
-        if (file) {
-            await firebase.SavePhoto(file, data._id);
-            await db.AddWork(data);
+        try {
+            await db.connect();
+            if (file) {
+                await firebase.SavePhoto(file, data._id);
+                await db.AddWork(data);
+            }
+        } finally {
+            await db.disconnect();
         }
     }
 );
@@ -55,14 +68,25 @@ app.post("/add_work", upload.single("img"), async (req: Request, res: Response) 
     }
 });*/
 
-app.post("/edit_work", upload.single("img"), async (req: Request, res: Response) => {
-    const data = req.body;
-    const file = req.file;
-    if (file) {
-        await firebase.UpdatePhoto(file, data._id);
-        await db.UpdateWork(data);
+app.post(
+    "/edit_work",
+    upload.single("img"),
+    async (req: Request, res: Response) => {
+        const data = req.body;
+        const file = req.file;
+        try {
+            await db.connect();
+            if (file) {
+                await firebase.UpdatePhoto(file, data._id);
+                await db.UpdateWork(data);
+            } else {
+                await db.UpdateWork(data);
+            }
+        } finally {
+            db.disconnect();
+        }
     }
-});
+);
 
 /*app.post("/edit_offer", async (req: Request, res: Response) => {
     const data = req.body;
@@ -75,8 +99,13 @@ app.post("/edit_work", upload.single("img"), async (req: Request, res: Response)
 
 app.post("/delete_work/:_id", async (req: Request, res: Response) => {
     const _id = req.params._id;
-    await db.DeleteWork(_id);
-    await firebase.DeletePhoto(_id);
+    try {
+        await db.connect();
+        await db.DeleteWork(_id);
+        await firebase.DeletePhoto(_id);
+    } finally {
+        await db.disconnect();
+    }
 });
 
 /*app.post("/delete_offer/:_id", async (req: Request, res: Response) => {
@@ -86,16 +115,12 @@ app.post("/delete_work/:_id", async (req: Request, res: Response) => {
 
 app.post("/sendEmail", (req: Request, res: Response) => {
     const { nameproject, email, offer } = req.body;
-    const mailerService = new MailerService(
-        nameproject,
-        email,
-        offer,
-    );
+    const mailerService = new MailerService(nameproject, email, offer);
     const result = mailerService.sendEmail();
-    res.send(result)
+    res.send(result);
 });
 
-app.post('/save_photo/:_id', async (req: Request, res: Response) => {
+app.post("/save_photo/:_id", async (req: Request, res: Response) => {
     const _id = req.params._id;
     const file = req.file;
     if (file) {
@@ -103,30 +128,34 @@ app.post('/save_photo/:_id', async (req: Request, res: Response) => {
     }
 });
 
-app.post('/save_photo_skill/:_id', upload.single("img"), async (req: Request, res: Response) => {
-    const _id = req.params._id;
-    const file = req.file;
-    console.log(file, _id)
-    if (file) {
-        await firebase.SavePhotoSkill(file, _id);
+app.post(
+    "/save_photo_skill/:_id",
+    upload.single("img"),
+    async (req: Request, res: Response) => {
+        const _id = req.params._id;
+        const file = req.file;
+        console.log(file, _id);
+        if (file) {
+            await firebase.SavePhotoSkill(file, _id);
+        }
     }
-});
+);
 
-app.post('/delete_photo/:_id', async (req: Request, res: Response) => {
+app.post("/delete_photo/:_id", async (req: Request, res: Response) => {
     const _id = req.params._id;
     await firebase.DeletePhotoSkill(_id);
 });
 
-app.get('/photos', async (req: Request, res: Response) => {
+app.get("/photos", async (req: Request, res: Response) => {
     const photo = await firebase.getAllPhotoURLs();
-    res.json(photo)
+    res.json(photo);
 });
 
 app.listen(5500, async () => {
-    await db.connect()
+    await db.connect();
     console.log(`http//:localhost:${5500}`);
 });
 
 app.on("exit", async () => {
-    await db.disconnect()
-})
+    await db.disconnect();
+});
